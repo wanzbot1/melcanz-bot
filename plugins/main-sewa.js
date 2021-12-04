af81f2c4b3b4de31aebb108bf5a5a04aded6fcb0
Amel <91192362+melcanz@users.noreply.github.com>
2021-12-04 11:56:36 +0700

    Update main-sewa.js

--- plugins/main-sewa.js
+++ plugins/main-sewa.js
@@ -3,17 +3,17 @@ let handler = async (m, { conn }) => {
     pepe = await conn.getProfilePicture('62831287340122@s.whatsapp.net').catch(() => 'https://i.ibb.co/jr9Nh6Q/Thumb.jpg')
     baper = await fetch(pepe).then(a => a.buffer())
     let listMessage = {
-        "title": "STIKERIN",
+        "title": "Melcanz-Bot",
         "description": `
-STIKERIN PRICE
+Melcanz PRICE
         
 1 Grup / 30 Hari
-Rp. 10,000 Dana, Ovo
-Rp. 15,000 Pulsa Axis
+Rp. 15,000 Dana, Ovo
+Rp. 20,000 Pulsa Axis/XL
 
 1 Premium / sampai pensi
-Rp. 10,000 Dana, Ovo
-Rp. 15,000 Pulsa Axis
+Rp. 20,000 Dana, Ovo
+Rp. 25,000 Pulsa Axis/XL
 `.trim(),
         "listType": "PRODUCT_LIST",
         "productListInfo": {
@@ -33,7 +33,7 @@ Rp. 15,000 Pulsa Axis
             },
             "businessOwnerJid": "6283128734012@s.whatsapp.net"
         },
-        "footerText": "hubungi wa.me/6283128734012"
+        "footerText": "hubungi wa.me/6283128734012 atau wa.me/6287755080455"
     }
 
 
@@ -41,6 +41,6 @@ Rp. 15,000 Pulsa Axis
 }
 handler.help = ['sewabot']
 handler.tags = ['main']
-handler.command = /^sewa(bot|stikerin)$/i
+handler.command = /^sewa(bot|melcanz)$/i
 
 module.exports = handler
